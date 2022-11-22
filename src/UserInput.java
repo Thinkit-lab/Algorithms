733d850e34d18053b3c77b4eb7e184c356006d09
Olukunle Afolabi <59313880+Thinkit-lab@users.noreply.github.com>
2022-11-22 23:34:08 +0100

    CHORE Counting Duplicate

---
+++ src/UserInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c;
+        a = input.nextInt();
+        b = input.nextInt();
+        c = a + b;
+        System.out.println(c);
+    }
+}
